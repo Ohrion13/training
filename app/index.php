276058a11dc778314dc1191d237cf1757b835a95
mickael <mortharion13@gmail.com>
2024-08-19 11:29:07 +0200

    save progress

--- app/index.php
+++ app/index.php
@@ -1,21 +1,4 @@
 <?php
-$text = 'Hello World!';
+// index.php
+echo "Hello, CI/CD!";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>
-        <?php
-        echo $text;
-        ?>
-    </h1>
-</body>
-
-</html>
